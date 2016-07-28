08a5cccf718aec810474194f22d27157757a85c9
honestraj <honestraj.it@gmail.com>
2016-07-28 15:54:06 +0530

    assessmentformvalidation

--- app/models/interviewerfeedback.rb
+++ app/models/interviewerfeedback.rb
@@ -1,5 +1,5 @@
 class Interviewerfeedback < ActiveRecord::Base
-   validates_presence_of :interviewername,:candidatename,:feedback,:status
+   validates_presence_of :interviewername,:candidatename,:status
    has_attached_file :attachedfile, styles: {thumbnail: "60x60#"}
    validates_attachment :attachedfile, content_type: { content_type: "application/pdf" }
 end
