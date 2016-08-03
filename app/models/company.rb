e0cc87528c6607def7219f207cabe8dc047ce9b9
honestraj <honestraj.it@gmail.com>
2016-08-03 14:42:06 +0530

    company bcrypt authentication

--- app/models/company.rb
+++ app/models/company.rb
@@ -3,7 +3,7 @@ class Company < ActiveRecord::Base
 #  validates_uniqueness_of :companyemail
   validates_format_of :companyemail, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
   validates_confirmation_of  :password
-  # has_secure_password
+ # has_secure_password
   scope :allwhere,->(companyemail,password){where("companyemail=? AND password=? ",companyemail,password)}
 
 end
