85fa3d627378087c652b8e84fc197ea92d555537
honestraj <honestraj.it@gmail.com>
2016-08-08 16:59:31 +0530

    Login Mailer Setup

--- app/mailers/user_notifier.rb
+++ app/mailers/user_notifier.rb
@@ -3,7 +3,14 @@ class UserNotifier < ActionMailer::Base
  # send a signup email to the user, pass in the user object that   contains the user's email address
  def send_signup_email(user)
    @user = user
-   mail( :to => @user.companyemail,:from => "honestraj.it@gmail.com",:subject => 'Thanks for Siginup Interviewapp',:body=>"Hi,\nThank you for Login Interview app URL:https://interviewerapp.herokuapp.com/" )
+   mail( :to => @user.companyemail,:from => "honestraj.it@gmail.com",:subject => 'Thanks for Siginup Interviewapp',
+   :body=>"Hi,\nThank you for Login Interview app URL:https://interviewerapp.herokuapp.com/
+
+          Login Credential:
+          =================   
+          Comapanymail: "+@user.companyemail+"
+          Password    : "+@user.password+"
+           " )
  end
 
 end
