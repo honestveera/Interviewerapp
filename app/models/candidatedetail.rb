39f42787a706348e770dfd9e143d64667f3ec778
honestraj <honestraj.it@gmail.com>
2016-08-16 15:48:30 +0530

    Email validation

--- app/models/candidatedetail.rb
+++ app/models/candidatedetail.rb
@@ -2,7 +2,7 @@ class Candidatedetail < ActiveRecord::Base
   #Validation
   has_attached_file :attachedfile, styles: {thumbnail: "60x60#"}
   validates_attachment :attachedfile, content_type: { content_type: "application/pdf" }
-  validates_presence_of :jobdescription
+  # validates_presence_of :jobdescription
   #Association
   has_many  :candidateprofiles
   has_many  :skillsets
