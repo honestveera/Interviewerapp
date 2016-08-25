6e4fbf869818713c1213484ddf880d907c23a2ee
honestraj <honestraj.it@gmail.com>
2016-08-25 16:52:48 +0530

    addcommentIFF

--- db/migrate/20160728053633_add_relevanceexperience_to_interviewerfeedback.rb
+++ db/migrate/20160728053633_add_relevanceexperience_to_interviewerfeedback.rb
@@ -1,5 +1,5 @@
 class AddRelevanceexperienceToInterviewerfeedback < ActiveRecord::Migration
   def change
-    add_column :interviewerfeedbacks, :relevanceexperience, :integer
+    add_column :interviewerfeedbacks, :relevanceexperience, :text
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160812091015) do
+ActiveRecord::Schema.define(version: 20160825073125) do
 
   create_table "candidatedetails", force: :cascade do |t|
     t.string   "candidatename"
@@ -63,31 +63,64 @@ ActiveRecord::Schema.define(version: 20160812091015) do
     t.decimal  "contactnumber"
   end
 
+  create_table "delayed_jobs", force: :cascade do |t|
+    t.integer  "priority",         default: 0, null: false
+    t.integer  "attempts",         default: 0, null: false
+    t.text     "handler",                      null: false
+    t.text     "last_error"
+    t.datetime "run_at"
+    t.datetime "locked_at"
+    t.datetime "failed_at"
+    t.string   "locked_by"
+    t.string   "queue"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "progress_stage"
+    t.integer  "progress_current", default: 0
+    t.integer  "progress_max",     default: 0
+  end
+
+  add_index "delayed_jobs", ["priority", "run_at"], name: "delayed_jobs_priority"
+
   create_table "interviewerfeedbacks", force: :cascade do |t|
     t.string   "interviewername"
     t.string   "candidatename"
     t.string   "status"
-    t.datetime "created_at",                null: false
-    t.datetime "updated_at",                null: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
     t.string   "skills"
     t.string   "attachedfile_file_name"
     t.string   "attachedfile_content_type"
     t.integer  "attachedfile_file_size"
     t.datetime "attachedfile_updated_at"
     t.integer  "relevanceexperience"
+    t.text     "relevanceexperiencecomment"
     t.integer  "personality"
+    t.text     "personalitycomment"
     t.integer  "communication"
+    t.text     "communicationcomment"
     t.integer  "technicalskills"
+    t.text     "technicalskillscomment"
     t.integer  "analyticalskills"
+    t.text     "analyticalskillscomment"
     t.integer  "jobknowledge"
+    t.text     "jobknowledgecomment"
     t.integer  "timemanagement"
+    t.text     "timemanagementcomment"
     t.integer  "commitementtask"
+    t.text     "commitementtaskcomment"
     t.integer  "leadership"
+    t.text     "leadershipcomment"
     t.integer  "teamwork"
+    t.text     "teamworkcomment"
     t.integer  "goalsettings"
+    t.text     "goalsettingscomment"
     t.integer  "creativity"
+    t.text     "creativitycomment"
     t.integer  "flexibility"
+    t.text     "flexibilitycomment"
     t.integer  "assertiveness"
+    t.text     "assertivenesscomment"
     t.string   "scheduletime"
     t.string   "scheduledate"
     t.float    "overallassesment"
