62dba1850964b5ad5dd5fc0a7f50b077164bbd51
honestraj <honestraj.it@gmail.com>
2016-09-13 14:35:22 +0530

    master

--- app/controllers/calenders_controller.rb
+++ app/controllers/calenders_controller.rb
@@ -1,5 +1,6 @@
 class CalendersController < ApplicationController
   before_action :set_calender, only: [:show, :edit, :update, :destroy]
+
   respond_to :html,:json
   # GET /calenders
   # GET /calenders.json
