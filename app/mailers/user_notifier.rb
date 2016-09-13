f33df51f499e0695ccdf960674389beb68101419
honestraj <honestraj.it@gmail.com>
2016-09-13 17:49:34 +0530

    Mailerchange

--- app/mailers/user_notifier.rb
+++ app/mailers/user_notifier.rb
@@ -4,7 +4,7 @@ class UserNotifier < ActionMailer::Base
  def send_signup_email(user)
    @user = user
    mail( :to => @user.companyemail,:from => "honestraj.it@gmail.com",:subject => 'Thanks for Siginup Interviewapp',
-   :body=>"Hi,\nThank you for Login Interview app URL:https://interviewerapp.herokuapp.com/
+   :body=>"Hi,\nThank you for Login Interview app URL:https://interviewerapps.herokuapp.com/
 
           Login Credential:
           =================
@@ -16,7 +16,7 @@ class UserNotifier < ActionMailer::Base
  def send_forgotpassword(user)
    @user = user[0]
    mail( :to => @user.companyemail,:from => "honestraj.it@gmail.com",:subject => 'Thanks for Siginup Interviewapp',
-   :body=>"Hi,\n  Your Password: URL:https://interviewerapp.herokuapp.com/
+   :body=>"Hi,\n  Your Password: URL:https://interviewerapps.herokuapp.com/
 
           Login Credential:
           =================
